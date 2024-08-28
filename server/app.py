c9339316288429239ec35fa49792bc5feb4d6bff
Rachel Katz <rachel.katz@flatironschool.com>
2024-08-28 14:31:33 -0400

    demo done

--- server/app.py
+++ server/app.py
@@ -1,16 +1,19 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
 
+#instance of api
 app = Flask(__name__)
+# app.db will be created when we do `flask db init`
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+# instance that allows us to migrate models into sql db (app.db)
 migrate = Migrate(app, db)
 db.init_app(app)
 
@@ -20,7 +23,30 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+# Add routes here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    q = Earthquake.query.filter_by(id=id).first()
+    # checking if query did not find anything (q is None)
+    if(not q):
+        return {'message': f'Earthquake {id} not found.'}, 404
+    #SerializerMixing allows us to use .to_dict()
+    return jsonify(q.to_dict()), 200
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquake_by_mag(magnitude):
+    #return a list of Earthquake instances
+    q = Earthquake.query.filter(Earthquake.magnitude>=magnitude).all()
+    #turning each Earthquake instance into a dictionary (so that we can jsonify it)
+    #.to_dict() is available thanks to SerializerMixin (see models.py)
+    q_dict = [quake.to_dict() for quake in q]
+    ret_json = {
+        #manually creating the "count" field
+        "count": len(q_dict),
+        "quakes": q_dict
+    }
+    return jsonify(ret_json), 200
+    
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -4,6 +4,18 @@ from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData()
 
+# instance of ORM
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+#SerializerMixin allows us to use .to_dict()
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = "earthquakes" 
+
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>'
